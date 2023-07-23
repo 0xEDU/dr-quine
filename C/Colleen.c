43d76e92e97329ad7bd6789267b4d5e700199c55
etachott <bfwaend@gmail.com>
2023-07-23 20:33:51 -0300

    feat: add sketch of a quine in Coleen.c

---
+++ C/Colleen.c
@@ -0,0 +1,10 @@
+/*
+   a
+*/
+#include<stdio.h>
+void b(){char*c="/*\n   a\n*/\n#include<stdio.h>\nvoid b(){char*c=%c%s%2$c;printf(c,10,34,";printf(c,10,34,c);}
+int main(void) {
+/*
+   b
+*/
+b();}
