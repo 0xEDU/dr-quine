1f65382eb8047f9987d7d20e96416f3b9ee5a2e2
edu <bfwaend@gmail.com>
2025-03-16 13:04:36 -0300

    Fixed colleen for non-Mac machines

--- ASM/colleen/Colleen.s
+++ ASM/colleen/Colleen.s
@@ -1,24 +1,34 @@
 ;a
-section .rodata:
-fmt:db ";a%csection .rodata:%cfmt:db %c%s%c,0%csection .text:%cglobal _start%cextern printf%cextern exit%c_f:%clea rdi,[rel fmt]%cmov rsi,10%cmov rdx,10%clea r8,[rel fmt]%cmov r9,34%cmov ecx,22%cpush_loop:%cpush 10%cdec ecx%cjnz push_loop%cmov rcx,34%cmov rax,0%ccall printf%ccall exit%c_start:%ccall _f;b%c",0
-section .text:
-global _start
+section .rodata
+fmt db ";a%csection .rodata%cfmt db %c%s%c,0%csection .text%cglobal main%cextern printf%cextern exit%c_f:%cpush rbp%cmov rbp,rsp%csub rsp,8%clea rdi,[rel fmt]%cmov rsi,10%cmov rdx,10%clea r8,[rel fmt]%cmov r9,34%cmov ecx,32%cpush_loop:%cpush 10%cdec ecx%cjnz push_loop%cxor eax,eax%cmov rcx,34%ccall printf WRT ..plt%cadd rsp,256%cadd rsp,8%cpop rbp%cret%cmain:%csub rsp,8%ccall _f;b%cmov rdi,0%ccall exit WRT ..plt%cadd rsp,8%c",0
+section .text
+global main
 extern printf
 extern exit
 _f:
+push rbp
+mov rbp,rsp
+sub rsp,8
 lea rdi,[rel fmt]
 mov rsi,10
 mov rdx,10
 lea r8,[rel fmt]
 mov r9,34
-mov ecx,22
+mov ecx,32
 push_loop:
 push 10
 dec ecx
 jnz push_loop
+xor eax,eax
 mov rcx,34
-mov rax,0
-call printf
-call exit
-_start:
+call printf WRT ..plt
+add rsp,256
+add rsp,8
+pop rbp
+ret
+main:
+sub rsp,8
 call _f;b
+mov rdi,0
+call exit WRT ..plt
+add rsp,8
